2a5ae17e14bfa30e5f3ff0591e19ae6fd3990fe6
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2019-11-10 17:27:40 +0700

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,29 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a[100],n,i,j,temp;
+	printf("How many elements: ");
+	scanf("%d",&n);
+	printf("Enter the element of array");
+	for(i=0;i<=n-1;i++)
+	{
+		for(j=0;j<=n-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	printf("Element of array after the sorting are:\n");
+	for(i=0;i<=n-1;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+	getch();
+	return 0;
+}
